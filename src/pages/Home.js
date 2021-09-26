1612954f51c88c26f2fb21158de16fe5b2beab2a
Your Name <hr5359@srmist.edu.in>
2021-09-26 22:09:20 +0530

    ✨ Home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react"
+import "./Home.css"
+import { Container, Row, Col } from "react-bootstrap";
+import HomeImage from "../assets/homeImg.png"
 
 export default function Home() {
     return (
         <div>
-            Home
+            <Container className="Intro">
+                <Row>
+                    <Col md={8} className="about-description">
+                        <h1>Traffic check</h1>
+                        <p>Scheduling a trip based on either when you'd like to leave for, or arrive to a desired location, so we came up with an idea of a feature that helps you avoid traffic hell.<br />
+                            We are given a public dataset which is useful to get timestamps and traffic rate for that time, our app basically gives the shortest path which will take least average time frame among other routes, to arrive at destination.
+                        </p>
+                    </Col>
+
+                    <Col md={4} className="img_traffic">
+                        <img src={HomeImage} className="img-fluid" alt="Me" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
